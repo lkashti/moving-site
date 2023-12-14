dd9f3b4eb4c8a53a32da762779671649b6ffddf1
Lior Kashti <lkashti@gmail.com>
2023-12-14 19:21:51 +0200

    first commit, added navbar

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = (props) => {
+  return (
+    <button className='bg-indigo-600 text-white rounded py-2 px-6 font-[Poppins] md:ml-8 hover:bg-indigo-400 duration-500'>{props.children}</button>
+  )
+}
+
+export default Button
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import Button from './Button'
+
+const NavBar = () => {
+    let Links = [
+        { name: "SERVICES", link: "" },
+        { name: "BUSINESS", link: "" },
+        { name: "ABOUT", link: "" },
+        { name: "CONTACT", link: "" },
+    ]
+    let [open,setOpen] = useState(false)
+    return (
+        <div className='shadow-md w-full fixed top-0 left-0'>
+            <div className='md:flex items-center justify-between bg-white py-3 md:px-10 px-7'>
+                <div className='font-bold text-2xl cursor-pointer flex items-center font-[Poppins] text-gray-800'>
+                    <span className='pt-1 text-indigo-500 mr-1'>
+                        <ion-icon name="business-outline"></ion-icon>
+                    </span>
+                    Moving
+                </div>
+                <div onClick={() => setOpen(!open)} className='text-3xl absolute right-8 top-4 cursor-pointer md:hidden '>
+                <ion-icon name={open? 'close':'menu-outline'}></ion-icon>
+                </div>
+                <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open? 'top-16 opacity-100':'top-[-490px] opacity-0'} md:opacity-100  `}>
+                    {
+                        Links.map(link => 
+                        <li key={link.name} className='md:ml-8 text-xl md:my-0 my-7'>
+                            <a href={link.link} className='text-gray-800 hover:text-gray-400 duration-500'>{link.name}</a>
+                        </li>
+                        )
+                    }
+                <Button>Sign Up</Button>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
