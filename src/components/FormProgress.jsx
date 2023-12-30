1c8789eed4ca47b0907f1094dd8ff2ffb2067bee
Lior Kashti <lkashti@gmail.com>
2023-12-30 18:58:25 +0200

    fixed progressbar

--- src/components/FormProgress.jsx
+++ src/components/FormProgress.jsx
@@ -1,16 +1,22 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const FormProgress = (props) => {
-    const [step, setStep] = useState(0)
-    const steps = [
-        { id: 1, "type": "From", current: true },
-        { id: 2, "type": "To", current: false },
-        { id: 3, "type": "Item List", current: false }
-    ]
+    // const [step, setStep] = useState(0)
+    const [steps, setSteps] = useState([
+        { id: 1, type: "From", current: false },
+        { id: 2, type: "To", current: false },
+        { id: 3, type: "Item List", current: false }
+    ])
+    useEffect(() => {
+        setSteps(steps.map(({ id, type, current }) => (
+            id == props.step + 1 ? { id, type, current: true } : { id, type, current: false }
+        )));
+        console.log(steps);
+    }, [props.step]);
 
     return (
         <ul className='flex flex-nowrap'>
-            {steps.map(step => <li className={`border h-full w-full text-center justify-center p-2 ${step.current?'bg-green-400':'bg-gray-400'} `} key={step.id}>{step.type}</li>)}
+            {steps.map(({ id, type, current }) => <li className={`border h-full w-full text-center justify-center p-2 ${current ? 'bg-green-400' : 'bg-gray-400'} `} key={id}>{type}</li>)}
 
         </ul>
     )
--- src/pages/TransportationOrder.jsx
+++ src/pages/TransportationOrder.jsx
@@ -7,7 +7,6 @@ const TransportationOrder = () => {
         <CardContainer>
             <MultiStepForm />
         </CardContainer>
-
     )
 }
 
